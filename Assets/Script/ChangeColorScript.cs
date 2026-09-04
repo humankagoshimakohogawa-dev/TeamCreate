3fe69e04173efb29eed0018a0fe11bb531968eb4
koh-admin <human.kagoshima.koh.ogawa@gmail.com>
2026-09-04 15:56:15 +0900

    ChoiceZone用colorChangeスクリプト追加

---
+++ Assets/Script/ChangeColorScript.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class ChangeColorScript : MonoBehaviour
+{
+    public Material MyColor;
+    
+    void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.color = Color.aliceBlue;
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = Color.gray;
+    }
+
+    void OnMouseDown()
+    {
+        GetComponent<Renderer>().material.color = Color.yellow;
+    }
+
+    void OnMouseUp()
+    {
+        GetComponent<Renderer>().material.color = Color.orange;
+    }
+}
